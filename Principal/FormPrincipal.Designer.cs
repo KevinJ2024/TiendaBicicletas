9255ad61b58122ea32c3df5fe950ac8a4ba49f95
Sena <malzatej257@gmail.com>
2025-03-12 16:29:39 -0500

    first commit

---
+++ Principal/FormPrincipal.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Principal
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Titulo = new Label();
+            SuspendLayout();
+            // 
+            // Titulo
+            // 
+            Titulo.AutoSize = true;
+            Titulo.Location = new Point(250, 74);
+            Titulo.Name = "Titulo";
+            Titulo.Size = new Size(165, 15);
+            Titulo.TabIndex = 0;
+            Titulo.Text = "Tienda Bicicletas - Back Office";
+            // 
+            // FormPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Titulo);
+            Name = "FormPrincipal";
+            Text = "FormPrincipal";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Titulo;
+    }
+}
