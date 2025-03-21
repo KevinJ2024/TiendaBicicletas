1adfead54621a9ee24c922097e86aa4b9628cc8c
Your Username <magoxaire@gmail.com.com>
2025-03-21 13:32:47 -0500

    Descripción de los cambios

--- Modelo/obj/Debug/net8.0/Modelo.AssemblyInfo.cs
+++ Modelo/obj/Debug/net8.0/Modelo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modelo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9255ad61b58122ea32c3df5fe950ac8a4ba49f95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f019a527167b402933ef956956065245389003ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modelo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modelo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
